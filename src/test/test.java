b453d8ef5fe1899d2f8f8ed9a44bea0b43acf106
Demon <598044692@qq.com>
2021-03-24 23:14:18 +0800

    test

---
+++ src/test/test.java
@@ -0,0 +1,14 @@
+package test;
+
+import java.nio.channels.ScatteringByteChannel;
+import java.util.Scanner;
+
+public class test {
+public static void main(String[] args)
+{
+ Scanner a = new Scanner(System.in);
+ double r = a.nextDouble();
+ double area = r * r * 3.14;
+ System.out.print(area);
+}
+}
